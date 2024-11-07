f7944fab0e1400ed7eecc1d233826e4f36d0b03b
Gideon Wiersma <g.j.wiersma@student.rug.nl>
2024-11-07 14:16:07 +0100

    added docstrings to model

--- autoop/core/ml/model/model.py
+++ autoop/core/ml/model/model.py
@@ -13,12 +13,20 @@ HyperParamType = Any
 
 
 class Model(Artifact):
+    """Artifact that represents the a model and its parameters."""
     def __init__(
-        self,
-        name: str,
-        asset_path: str,
-        version: str = "v0.00"
-    ):
+            self,
+            name: str,
+            asset_path: str,
+            version: str = "v0.00"
+        ):
+        """Initialise a Model artifact.
+
+        Args:
+            name (str): Name of the artifact
+            asset_path (str): Location where the data is allowed to be stored
+            version (str, optional): Version. Defaults to "v0.00".
+        """
         super().__init__(
             type="model",
             name=name,
@@ -26,7 +34,18 @@ class Model(Artifact):
             version=version
         )
 
-    def save(self, params: np.ndarray, hyperparams: np.ndarray):
+    def save(self, params: np.ndarray, hyperparams: np.ndarray) -> bytes:
+        """
+        Save parameters (usually the weights of the model) and the
+        hyperparameters (the knobs for traing) into the artifact.
+
+        Args:
+            params (np.ndarray): Parameters (weights) of the model.
+            hyperparams (np.ndarray): Hyperparameters of the model.
+
+        Returns:
+            bytes: bytes representation of the data that is stored.
+        """
         dict_ = {
             "params": params,
             "hyperparams": hyperparams
@@ -35,6 +54,11 @@ class Model(Artifact):
         return super().save(bytes)
 
     def read(self) -> dict[str, np.ndarray]:
+        """Read the data of the artifact. 
+
+        Returns:
+            dict[str, np.ndarray]: Dictionarry with params and hyperparams key
+        """
         bytes = super().read()
         dict_ = pickle.loads(bytes)
         return dict_
