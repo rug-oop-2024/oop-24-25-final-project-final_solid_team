8047ef8b9078d8b21f12c26f482f882658fb024b
Gideon Wiersma <g.j.wiersma@student.rug.nl>
2024-11-09 08:35:59 +0100

    style

--- autoop/core/ml/artifact.py
+++ autoop/core/ml/artifact.py
@@ -48,36 +48,36 @@ class Artifact:  # Original had Pydantic
         """
         self.data = binary_string
         return binary_string
-    
+
     @property
     def id(self) -> str:
         """Get the id of this artifact."""
         return str(base64.b64encode(self.asset_path.encode())) + self.version
-    
+
     @property
     def type(self) -> str:
         return self._type
-    
+
     @property
     def name(self) -> str:
         return self._name
-    
+
     @property
     def version(self) -> str:
         return self._version
-    
+
     @property
     def tags(self) -> list[str]:
         return self._tags
-    
+
     @property
     def metadata(self) -> dict[str, str]:
         return self._metadata
-    
+
     @property
     def asset_path(self) -> str:
         return self._asset_path
-    
+
     @property
     def data(self) -> bytes:
         return self._data
@@ -115,6 +115,3 @@ class Artifact:  # Original had Pydantic
 # Remarks:
 # We internally use the getters of the attributes such we can later change
 # how these atributes are stored
-
-
-		
