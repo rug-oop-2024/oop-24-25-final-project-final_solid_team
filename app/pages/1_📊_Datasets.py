08d5df6f62d129264d579d25a1b9f4729aa4fefb
Gideon Wiersma <g.j.wiersma@student.rug.nl>
2024-11-09 15:27:16 +0100

    another renaming

--- app/pages/1_📊_Datasets.py
+++ app/pages/1_📊_Datasets.py
@@ -5,7 +5,7 @@ import streamlit as st
 from sklearn.datasets import load_iris
 
 from app.core.system import AutoMLSystem
-from app.core.handle_datasets import DatasetHandler
+from app.core.dataset_handler import DatasetHandler
 from autoop.core.ml.dataset import Dataset
 from autoop.core.ml.dataset import Artifact
 
