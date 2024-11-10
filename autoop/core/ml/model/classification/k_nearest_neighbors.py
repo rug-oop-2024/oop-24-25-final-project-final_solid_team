bd3748c7430e68765d29b0cb3a68328edf974faf
resu <resu@example.com>
2024-11-10 22:42:19 +0100

    Docstrings

--- autoop/core/ml/model/classification/k_nearest_neighbors.py
+++ autoop/core/ml/model/classification/k_nearest_neighbors.py
@@ -3,14 +3,21 @@ from numpy.typing import ArrayLike
 from sklearn.neighbors import KNeighborsClassifier
 
 from autoop.core.ml.model.model import Model, ParametersDict
+from autoop.core.ml.artifact import Artifact
 
 
 class WrapKNearestNeighbors(Model):
+    """Wrapper for the K nearest neighbor model
+    from sklearn. Child of model class
+    """
     def __init__(
             self,
             parameters: dict = ParametersDict({}),
             hyper_parameters: dict = ParametersDict({}),
     ) -> None:
+        """Intiaialise k nearest neighbor model
+        Initialise as classification type model"""
+
         super().__init__(
             type="classification",
             hyper_parameters=ParametersDict(hyper_parameters),
@@ -20,6 +27,7 @@ class WrapKNearestNeighbors(Model):
         self._model = KNeighborsClassifier(**hyper_parameters)
 
     def fit(self, X: ArrayLike, y: ArrayLike) -> None:
+        """Fit k nearest neighbor model"""
         self._model.fit(X, y)
         self._parameters.update({
             "classes": self._model.classes_,
@@ -30,12 +38,16 @@ class WrapKNearestNeighbors(Model):
         })
 
     def predict(self, X: np.ndarray) -> np.ndarray:
+        """Predict target features of input using
+          fitting k nearest neighbors model"""
         assert self._parameters["classes"] is not None, (
             "Model is not fitted yet!"
         )
         return self._model.predict(X)
 
-    def to_artifact(self, asset_path="./assets/models", version="v0.00"):
+    def to_artifact(self, asset_path: str = "./assets/models",
+                    version: str = "v0.00") -> Artifact:
+        """Convert k nearest neighbor model to artifact"""
         return super().to_artifact(
             name="k nearest neighbor model",
             asset_path=asset_path,
--- autoop/core/ml/model/classification/random_forest.py
+++ autoop/core/ml/model/classification/random_forest.py
@@ -3,18 +3,20 @@ from numpy.typing import ArrayLike
 from sklearn.ensemble import RandomForestClassifier
 
 from autoop.core.ml.model.model import Model, ParametersDict
+from autoop.core.ml.artifact import Artifact
 
 
 class WrapRandomForest(Model):
+    """Wrapper for the sklearn random forest class
+    child of the general model class"""
     def __init__(
             self,
             params: dict = ParametersDict({}),
             hyper_params: dict = ParametersDict({}),
-        ) -> None:
-        """_summary_
-
-        Args:
-                tba
+    ) -> None:
+        """
+        Wrapper for the sklearn random forest class
+        initialize as classification type model.
           """
         super().__init__(
             type="classification",
@@ -25,6 +27,8 @@ class WrapRandomForest(Model):
         self._model = RandomForestClassifier(**hyper_params)
 
     def fit(self, X: ArrayLike, y: ArrayLike) -> None:
+        """Fit random forest model to set
+        of input and target features"""
         self._model.fit(X, y)
         self._params.update({
             "estimator": self._model.estimator_,
@@ -33,12 +37,16 @@ class WrapRandomForest(Model):
         })
 
     def predict(self, X: np.ndarray) -> np.ndarray:
+        """Predict target features based on
+        fitted random forest model"""
         assert self._params["estimator"] is not None, (
             "Model is not fitted yet!"
         )
         return self._model.predict(X)
 
-    def to_artifact(self, asset_path="./assets/models", version="v0.00"):
+    def to_artifact(self, asset_path: str = "./assets/models",
+                    version: str = "v0.00") -> Artifact:
+        """Covert random forest model to artifact"""
         return super().to_artifact(
             name="random forest model",
             asset_path=asset_path,
